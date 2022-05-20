3179156896969faed9d0ff1b97813990bee69b18
David Bautista <davidbautista@swuser-129-8-219-130.swlan.csufresno.edu>
2022-05-19 17:42:08 -0700

    finish code

--- finalclass_average.py
+++ finalclass_average.py
@@ -19,3 +19,18 @@ loop through scores
 average = accumulator / total score
 print average
 """
+
+def calculate_average(scores):
+    iterator = 0
+    accumulator = 0 
+    student_count = len(scores)
+    while iterator < len(scores):
+        accumulator = accumulator + scores [iterator]
+        iterator = iterator + 1
+    average = accumulator / student_count
+    print(average)
+    return average
+output = calculate_average
+output = calculate_average([78,67,56,99,80,83,82,91,94,95,77,88,85,92,91,79,88,82,81,86,94,93,92,45])
+
+print("output", output)
